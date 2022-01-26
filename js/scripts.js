badaa3852fc078e58f75585eb44f56b113400956
Matt Eilar <meilar@gmail.com>
2022-01-26 12:02:54 -0800

    add ui function to end turn button
    Co-authored-by: Matt Eilar <meilar@gmail.com>
    Co-authored-by: Daniel Lindsey <daniellindsey85254@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,6 @@ function rollDice() {
     return num + 1;
 }
 
-//rollDice()
-
 function Player(id) {
   this.score = 0;
   this.id = id;
@@ -39,16 +37,48 @@ function scoreKeeper(roll) {
   return thisTurnScore;
 }
 
+function addScore() {
+  if (player1.isTurn === true) {
+    player1.score = player1.score + thisTurnScore;  
+  } else if (player2.isTurn === true) {
+    player2.score = player2.score + thisTurnScore;
+  }
+  thisTurnScore = 0;
+}
+
 // UI Logic
+
+function checkForFinish() {
+  let score1 = player1.score;
+  let score2 = player2.score;
+
+  if (score1 >= 100) {
+    $("#card-1").addClass("winner");
+    $("#card-1").removeClass("active");
+    $("#stop-turn").addClass("hidden");
+    $("#restart").removeClass("hidden");
+    $("#player-1-win").removeClass("hidden");
+    $("#player-1-name").addClass("hidden");
+  } else if (score2 >= 100) {
+    $("#card-2").addClass("winner");
+    $("#card-2").removeClass("active");
+    $("#stop-turn").addClass("hidden");
+    $("#restart").removeClass("hidden");
+    $("#player-2-win").removeClass("hidden");
+    $("#player-2-name").addClass("hidden");
+  }
+}
 
 function turnRefresh() {
   if (player2.isTurn === true) {
-    $("#card-2").addClass("active");  
-    $("#card-1").removeClass("active");  
-  } 
+    $("#card-2").addClass("active");
+    $("#card-1").removeClass("active"); 
+    console.log(2); 
+  }
   if (player1.isTurn === true) {
     $("#card-1").addClass("active");
-    $("#card-2").removeClass("active");  
+    $("#card-2").removeClass("active");
+    console.log(1);  
   }
 }
 
@@ -61,7 +91,7 @@ function scoreRefresh() {
   scoreDisplay1.text(player1.score);
   scoreDisplay2.text(player2.score);
   turnDisplay.text(thisTurnScore);
-  turnRefresh();
+  
 }
 
 
@@ -70,13 +100,20 @@ $(document).ready(function() {
   scoreRefresh();
   $("#dice-img").click(function() {
     let thisTurn = rollDice();
+    $("#dice-img").attr("src","https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_"+ thisTurn + "-512.png")
     scoreKeeper(thisTurn);
     scoreRefresh();
+    turnRefresh();
   });
   $("#stop-turn").click(function() {
-    scoreRefresh()
+    addScore();
+    changeActivePlayer();
+    checkForFinish();
+    scoreRefresh();
+    turnRefresh();
   });
   $("#restart").click(function() {
     scoreRefresh();
+    location.reload(true);
   });
 })
